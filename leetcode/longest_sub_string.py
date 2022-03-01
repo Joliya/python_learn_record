8b60fdabcc7ab1879505693c8405e8c6438d6e06
zhangjinpeng <zhangjinpeng@chunyu.me>
2022-03-01 17:32:48 +0800

    leetcode 3 无重复字符串最长子串

--- leetcode/longest_sub_string.py
+++ leetcode/longest_sub_string.py
@@ -6,7 +6,7 @@
 from __future__ import absolute_import, unicode_literals
 
 
-def lengthOfLongestSubstring(s):
+def lengthOfLongestSubstring1(s):
     """
     无重复最长子串
     :param s:
@@ -14,14 +14,23 @@ def lengthOfLongestSubstring(s):
     """
 
     length = len(s)
-    ss = []
+
+    if length < 2:
+        return length
+    r_len = 0
     for i in range(len(s)):
         sub = []
         sub.append(s[i])
         for j in range(i + 1, length):
             if s[j] in sub:
-                ss = sub
+                l = len(sub)
+                if l > r_len:
+                    r_len = l
                 break
             else:
                 sub.append(s[j])
-    return "".join(ss)
+        else:
+            l = len(sub)
+            if l > r_len:
+                r_len = l
+    return r_len
