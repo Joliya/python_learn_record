6871ebe3d5e6719e1efeec0a22d89677c98a2dfd
zhangjinpeng <zhangjinpeng@huixuanjiasu.com>
2024-07-09 20:15:42 +0800

    最小曼哈顿距离

--- baobao/test.py
+++ baobao/test.py
@@ -4,10 +4,37 @@
 @desc: 
 """
 
+import pandas as pd
 
-import numpy as np
 
-arr = np.array([-1, 0, 2, -3, 4])
-result = np.any(arr > 0)
-print(result)  # 输出：True
+def find_combinations(data, target, start=0, path=[], result=None):
+    if result is None:
+        result = []
+    # 如果目标小于0，或者起始位置超出范围，直接返回
+    if target < 0 or start >= len(data):
+        return
+    # 如果目标正好为0，添加当前路径到结果
+    if target == 0:
+        result.append(path)
+        print(result)
+        return result
+    for i in range(start, len(data)):
+        # 选择当前元素，递归寻找剩余的
+        find_combinations(data, target - data[i][1], i + 1, path + [data[i]], result)
+    return result
 
+# 示例数据
+
+data = pd.read_excel("/Users/zhangjinpeng/workspace/python_learn_record/baobao/执行明细-6月.xlsx")
+data.fillna("", inplace=True)
+data = [(i[0], i[1]) for i in data.values]
+
+# 目标数值
+target = 1132400
+
+# 查找组合
+combinations = find_combinations(data, target)
+
+# 打印结果
+for combination in combinations:
+    print(combination)
